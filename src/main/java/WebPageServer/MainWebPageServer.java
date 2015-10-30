aad254854ca1a27e111e85b575de3be4c4be8991
Scott Zhu <zhuhuihuihui@gmail.com>
2015-10-30 00:50:55 -0700

    init project

---
+++ src/main/java/WebPageServer/MainWebPageServer.java
@@ -0,0 +1,14 @@
+package WebPageServer;
+
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
+/**
+ * Created by Scott on 10/30/15.
+ */
+public class MainWebPageServer {
+    public static void main(String[] args) {
+        port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
+    }
+}
