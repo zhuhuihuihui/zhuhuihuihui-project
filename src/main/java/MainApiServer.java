4b825c2099ec19cfb10d0b4e156f588aec3f7630
Scott Zhu <zhuhuihuihui@gmail.com>
2015-12-16 23:58:53 -0800

    change cros header

--- src/main/java/MainApiServer.java
+++ src/main/java/MainApiServer.java
@@ -8,11 +8,13 @@ import org.json.simple.JSONObject;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
+import spark.Spark;
 
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -21,11 +23,34 @@ import static spark.Spark.*;
  * Created by Scott on 10/30/15.
  */
 public class MainApiServer {
+    private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+
+    static {
+        corsHeaders.put("Access-Control-Allow-Methods", "HEAD, GET, POST, PUT, DELETE, OPTIONS, TRACE");
+        corsHeaders.put("Access-Control-Allow-Origin", "*");
+        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin, token");
+        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+    }
+
+    public final static void apply() {
+        Filter filter = new Filter() {
+            @Override
+            public void handle(Request request, Response response) throws Exception {
+                corsHeaders.forEach((key, value) -> {
+                    response.header(key, value);
+                    System.out.println("Header set!!!");
+                });
+            }
+        };
+        Spark.after(filter);
+    }
+
     public static void main(String[] args) {
         port(Integer.valueOf(System.getenv("PORT") == null ? "8080": System.getenv("PORT")));
         staticFileLocation("/public");
 
-        enableCORS("*", "HEAD, GET, POST, PUT, DELETE, OPTIONS, TRACE", "Content-Type, token");
+//        enableCORS("*", "HEAD, GET, POST, PUT, DELETE, OPTIONS, TRACE", "Content-Type, token");
+        MainApiServer.apply();
 
         Database.getInstance().getUserWithEmail("");
 
